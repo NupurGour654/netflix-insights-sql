31525ab62f2b0ecc189f7b130efdb51cd8d4dc15
NupurGour654 <155764854+NupurGour654@users.noreply.github.com>
2025-07-08 22:25:44 +0530

    Fetch SQL output into pandas DataFrames

--- charts.py
+++ charts.py
@@ -4,3 +4,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set(style="whitegrid")
+# Query runner to return DataFrame
+def fetch_df(query):
+    conn = sqlite3.connect("netflix.db")
+    df_result = pd.read_sql_query(query, conn)
+    conn.close()
+    return df_result
