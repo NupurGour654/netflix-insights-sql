8e1e7d7aa2fba62b097af510a4488bd1850c8fd6
NupurGour654 <155764854+NupurGour654@users.noreply.github.com>
2025-07-08 22:14:19 +0530

    Clean column names before saving to DB

--- setup_db.py
+++ setup_db.py
@@ -1,2 +1,7 @@
 import sqlite3
 import pandas as pd
+# Load CSV
+df = pd.read_csv("data/netflix.csv")
+
+# Clean column names
+df.columns = [col.strip().replace(" ", "_").lower() for col in df.columns]
