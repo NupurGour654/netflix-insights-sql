58b96b8a12812f68e3b0a06ddaa5b025bac12813
NupurGour654 <155764854+NupurGour654@users.noreply.github.com>
2025-07-08 22:20:58 +0530

    Add SQL query for type-wise content

--- analysis.py
+++ analysis.py
@@ -3,17 +3,24 @@ import sqlite3
 def run_query(query, desc):
     conn = sqlite3.connect("netflix.db")
     cursor = conn.cursor()
-    print(f"\n🔍 {desc}")
+    print(f"\n {desc}")
     for row in cursor.execute(query):
         print(row)
     conn.close()
 
 # Queries
-q1 = """
+q1 = 
 SELECT release_year, COUNT(*) as total
 FROM netflix
 WHERE release_year IS NOT NULL
 GROUP BY release_year
 ORDER BY release_year DESC
 LIMIT 10;
-"""
+
+
+q2 = 
+SELECT type, COUNT(*) as total
+FROM netflix
+GROUP BY type
+ORDER BY total DESC;
+
